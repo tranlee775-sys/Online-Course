e39050699f1bb943a733a58d3ffbd23e1fae4a37
unknown <huy@fpt.edu.vn>
2026-04-08 18:11:00 +0700

    Complete task 6: added urls for submit and results

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,26 +4,27 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # Route cho danh sách khóa học (trang chủ của app)
+    # Route cho trang chủ danh sách khóa học
     path(route='', view=views.CourseListView.as_view(), name='index'),
     
-    # Route cho đăng ký và đăng nhập
+    # Route cho registration, login và logout
     path('registration/', views.registration, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
 
-    # Route cho chi tiết khóa học: /onlinecourse/5/
+    # Route cho chi tiết khóa học: /onlinecourse/1/
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
 
     # ----------------------------------------------------------------------
-    # TASK 6: THÊM CÁC PATH CHO SUBMIT VÀ SHOW_EXAM_RESULT
+    # TASK 6: PATHS CHO SUBMIT VÀ SHOW_EXAM_RESULT
     # ----------------------------------------------------------------------
     
-    # Path để xử lý nộp bài thi
+    # Path xử lý nộp bài thi
     path('course/<int:course_id>/submit/', views.submit, name='submit'),
     
-    # Path để hiển thị kết quả sau khi nộp
+    # Path hiển thị kết quả sau khi nộp
     path('submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.DOCUMENT_ROOT)
