f7e02dbe609edbed2cf88cec65efae3b621d315c
unknown <huy@fpt.edu.vn>
2026-04-08 18:15:27 +0700

    Fix urls.py Task 6

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     # Path hiển thị kết quả sau khi nộp
     path('submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
-] + static(settings.MEDIA_URL, document_root=settings.DOCUMENT_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
